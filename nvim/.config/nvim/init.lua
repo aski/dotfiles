cf8e338002bab7254041f3ee49795febd9344977
aski <andy3j@gmail.com>
2023-05-27 16:37:43 +1200

    Update lua config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,51 +5,55 @@ vim.g.maplocalleader = ' '
 -- Install package manager
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system {
-		'git',
-		'clone',
-		'--filter=blob:none',
-		'https://github.com/folke/lazy.nvim.git',
-		'--branch=stable',
-		lazypath,
-	}
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Install plugins
 require('lazy').setup({
 
-	'tpope/vim-fugitive',
+  'tpope/vim-fugitive',
 
-	{
-		'shaunsingh/nord.nvim',
-		priority = 1000,
-		config = function ()
-			vim.cmd.colorscheme 'nord'
-		end,
-	},
+  {
+    'shaunsingh/nord.nvim',
+    priority = 1000,
+    config = function ()
+      vim.cmd.colorscheme 'nord'
+    end,
+  },
 
-	{
-		'nvim-lualine/lualine.nvim',
-		opts = {
-			options = {
-				icons_enabled = false,
-				theme = 'nord',
-				component_separators = '|',
-				section_separators = '',
-			},
-		},
-	},
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'nord',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
 
-	{
-		'nvim-treesitter/nvim-treesitter',
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter-textobjects',
-		},
-		build = ':TSUpdate',
-	},
-	
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
 
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+    },
+  },
 })
 
 -- [[ Setting options ]]
@@ -77,3 +81,5 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- Enable 24-bit RGB color in terminal
 vim.o.termguicolors = true
+
+-- vim: ts=2 sts=2 sw=2 et
