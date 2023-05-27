0044208fbdc41bc2273abed3f888dd93c31f44f1
aski <andy3j@gmail.com>
2023-05-27 16:29:43 +1200

    New init.lua config file

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,79 @@
+-- Set <space> as the leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Install package manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable',
+		lazypath,
+	}
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Install plugins
+require('lazy').setup({
+
+	'tpope/vim-fugitive',
+
+	{
+		'shaunsingh/nord.nvim',
+		priority = 1000,
+		config = function ()
+			vim.cmd.colorscheme 'nord'
+		end,
+	},
+
+	{
+		'nvim-lualine/lualine.nvim',
+		opts = {
+			options = {
+				icons_enabled = false,
+				theme = 'nord',
+				component_separators = '|',
+				section_separators = '',
+			},
+		},
+	},
+
+	{
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ':TSUpdate',
+	},
+	
+
+})
+
+-- [[ Setting options ]]
+
+-- Enable line numbers
+vim.wo.number = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+-- Better completion
+vim.o.completeopt = 'menuone,noselect'
+
+-- Enable 24-bit RGB color in terminal
+vim.o.termguicolors = true
