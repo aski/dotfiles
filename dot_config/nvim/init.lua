0ef7a70ce7146b572b7606e1222a2522bf8ef7b2
aski <andy3j@gmail.com>
2023-05-28 22:03:29 +1200

    Swap keybinds for git_files and find_files

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -140,8 +140,8 @@ require('telescope').setup {}
 
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').git_files, { desc = '[F]ind [F]iles' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').find_files, { desc = '[F]ind [G]it Files' })
+vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+vim.keymap.set('n', '<leader>fg', require('telescope.builtin').git_files, { desc = '[F]ind [G]it Files' })
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
 
 -- [[ Configure Treesitter ]]
