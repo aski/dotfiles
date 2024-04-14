a62538f5f2522a4e2c5e7b1e8dff4dd88dd44432
andy3j <andy3j@gmail.com>
2024-04-14 22:26:12 +1200

    Updates

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -322,6 +322,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua',
+        'tsserver',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
